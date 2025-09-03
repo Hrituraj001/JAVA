a66fca8d0883a02edf7c7df2846e95cd62716cb9
HRITURAJ YADAV <hrituraj.yadav_cs24@gla.ac.in>
2025-09-03 20:58:53 +0530

    main

---
+++ evenodd.java
@@ -0,0 +1,10 @@
+public class evenodd {
+  public static void main(String[] args) {
+    int number = 5;
+    if (number % 2 == 0) {
+      System.out.println("Even");
+    } else {
+      System.out.println("Odd");
+    }
+  }
+}
---
+++ public class trip.java
@@ -0,0 +1,28 @@
+public class trip
+{
+    public static void triplet(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                for(int k=j<=j<n.length;k++)
+                int c= arr[i]+arr[j]+arr[k];
+            }
+            if(c==0)
+            {
+                System.out.println("["+arr[i],arr[j],arr[k]+"]");
+            }
+            else
+            {
+                System.out.print("[]");
+            }
+        }
+    }
+    public static void main(String args[])
+    {
+        int arr[]={-1,0,1,2,4,1};
+        triplet(arr);
+    }
+
+}
